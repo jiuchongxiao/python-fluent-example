69ebc0f857c9375023c8d79798e6dc21f2fef398
admin <liujiabao1@126.com>
2018-05-25 10:33:07 +0800

    bug

--- fluent-python-logging-Asynchronous .py
+++ fluent-python-logging-Asynchronous .py
@@ -8,16 +8,12 @@ def overflow_handler(pendings):
     unpacker = msgpack.Unpacker(BytesIO(pendings))
     for unpacked in unpacker:
         print(unpacked)
-
-
 custom_format = {
   'host': '%(hostname)s',
   'where': '%(module)s.%(funcName)s',
   'type': '%(levelname)s',
   'stack_trace': '%(exc_text)s'
 }
-
-
 logging.basicConfig(level=logging.INFO)
 l = logging.getLogger('fluent.test')
 h = handler.FluentHandler('fluent-python.asyn-log', host='192.168.181.99', port=30224, buffer_overflow_handler=overflow_handler)
